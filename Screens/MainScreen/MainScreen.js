9f9a2bdf0f6cab85e4235772a6dc4e326ce55459
devrh <hathi.dev@namasyslimited.onmicrosoft.com>
2021-10-10 17:46:56 +0530

    added client side socket io

--- Screens/MainScreen/MainScreen.js
+++ Screens/MainScreen/MainScreen.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import VideoPlayerScreen from "./VideoPlayerScreen/VideoPlayerScreen";
 import ChatScreen from "./ChatScreen/ChatScreen";
+import { io } from "socket.io-client";
+
+const socket = io(`http://192.168.1.5:8000/`);
 
 export default function MainScreen({ route, navigation }) {
   const { urlText } = route.params;
+
+  useEffect(() => {
+    socket.on("connection", () => {
+      console.log(socket.id);
+    });
+  });
 
   const styles = StyleSheet.create({
     container: {
@@ -17,8 +26,6 @@ export default function MainScreen({ route, navigation }) {
 
   return (
     <View style={styles.container}>
-      {console.log(urlText)}
-
       <VideoPlayerScreen />
       <ChatScreen />
     </View>
